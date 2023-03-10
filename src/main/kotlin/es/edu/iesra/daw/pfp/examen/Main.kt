19192247ca53f6a87fedcd5ae3bc9b58547b4a0c
F0rno <pforper2412@g.educaand.es>
2023-03-10 12:11:13 +0100

    Mejoras en el main

--- src/main/kotlin/es/edu/iesra/daw/pfp/examen/Main.kt
+++ src/main/kotlin/es/edu/iesra/daw/pfp/examen/Main.kt
@@ -2,34 +2,27 @@ package es.edu.iesra.daw.pfp.examen
 
 import es.edu.iesra.daw.pfp.examen.dados.Dado
 import es.edu.iesra.daw.pfp.examen.dados.DadoHexaedro
+import es.edu.iesra.daw.pfp.examen.juegos.JuegoDeDados
 import es.edu.iesra.daw.pfp.examen.juegos.JuegoDeDadosChicago
 import es.edu.iesra.daw.pfp.examen.juegos.JuegoDeDadosSencillo
 
-fun main() {
-    /*
+fun configurarSencillo(tipoDeDado: Int, numeroDeJugadores: Int = 2) : JuegoDeDados {
     val jugadores = mutableListOf<Jugador>()
     val dados = mutableListOf<Dado>()
-    val numeroDeJugadores = 100
 
     repeat(3) {
-        dados.add(Dado(3U))
+        dados.add(Dado(tipoDeDado.toUByte()))
     }
 
     repeat(numeroDeJugadores) {
         jugadores.add(Jugador(it.toString()))
     }
+    return JuegoDeDadosSencillo(jugadores, dados, 1000)
+}
 
-    val miJuego = JuegoDeDadosSencillo(jugadores, dados, 1000)
-    miJuego.empezarJuego()
-    miJuego.mostrarGanador().forEach { jugador ->
-        println(jugador)
-    }
-    */
-
-
+fun configurarChicago(numeroDeJugadores: Int = 3) : JuegoDeDados {
     val jugadores = mutableListOf<Jugador>()
     val dados = mutableListOf<DadoHexaedro>()
-    val numeroDeJugadores = 10
 
     repeat(2) {
         dados.add(DadoHexaedro())
@@ -38,11 +31,22 @@ fun main() {
     repeat(numeroDeJugadores) {
         jugadores.add(Jugador(it.toString()))
     }
+    return JuegoDeDadosChicago(jugadores, dados)
+}
 
-    val miJuego = JuegoDeDadosChicago(jugadores, dados)
-    miJuego.empezarJuego()
-    miJuego.mostrarGanador().forEach { ganador ->
-        println(ganador)
+fun main() {
+    val miJuegoSencillo = configurarSencillo(3, 10)
+    miJuegoSencillo.empezarJuego()
+
+    miJuegoSencillo.mostrarGanador().forEach { jugador ->
+        println(jugador)
     }
 
+    println("########################")
+
+    val miJuegoChicago = configurarChicago(10)
+    miJuegoChicago.empezarJuego()
+    miJuegoChicago.mostrarGanador().forEach { ganador ->
+        println(ganador)
+    }
 }
